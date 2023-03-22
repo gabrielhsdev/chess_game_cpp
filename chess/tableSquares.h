355a742a4bf9e7e06c53145378c7a6dbfb7501c6
gabriel <henriquegabrielhs21@gmail.com>
2023-03-22 20:24:14 -0300

    Refactoring how square colors are set-up

--- chess/tableSquares.h
+++ chess/tableSquares.h
@@ -20,6 +20,11 @@ public:
 	int posXid = 0;
 	int posYid = 0;
 
+	sf::Color color_active;
+	sf::Color color_default;
+	sf::Color color_selected;
+	sf::Color color_action;
+
 	tableSquares();
 	void drawText(sf::RenderWindow* window);
 	void drawSquare(sf::RenderWindow* window, bool selected);
