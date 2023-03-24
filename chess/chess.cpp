25b6e6f05803921ad360b2b4aa6d0ca8c265dda1
gabriel <henriquegabrielhs21@gmail.com>
2023-03-24 14:58:58 -0300

    chess formatting

--- chess/chess.cpp
+++ chess/chess.cpp
@@ -1,5 +1,6 @@
 #include "boardStats.h"
 #include "gameController.h"
+#include <windows.h>
 #include<iostream>
 #include <SFML/Graphics.hpp>
 
@@ -11,36 +12,37 @@ extern int windowwidth = 1024;
 int main()
 {
     int squareSize = (windowwidth) / 8;
+
     gameController gameCtrl(windowwidth, windowheight);
-    gameCtrl.game_status = 2;
-    // create the window
     sf::RenderWindow window(sf::VideoMode(windowwidth, windowheight), "Not Chess", sf::Style::Titlebar | sf::Style::Close);
+    sf::Vector2i position;
     sf::err().rdbuf(NULL);
-    // run the program as long as the window is open
+
+    gameCtrl.game_status = 2;
+
+    //Run program
     while (window.isOpen())
     {
         window.clear();
 
-        // check all the window's events that were triggered since the last iteration of the loop
+        //Check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             window.setKeyRepeatEnabled(false);
-            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        //Example of mouse position being passes to other child objects of game controller
-        sf::Vector2i position = sf::Mouse::getPosition(window);
+        //Constantly get mouse position
+        position = sf::Mouse::getPosition(window);
         gameCtrl.mouse_position = position;
-        //cout << position.x << "-" << position.y << "\n";
 
-        //Build stuff here !
-        //myBoard.buildBoard(&window);
+        //Game controller starts here
         gameCtrl.print_game_status(&window);
 
-        // end the current frame
+        //End frame
         window.display();
     }
     return 0;
